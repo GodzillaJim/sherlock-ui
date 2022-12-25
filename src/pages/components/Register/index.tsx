a7b1668ab7c6a4ce3a23facc04d1516b31585a80
Jimna Njoroge <jimnanjoroge76@gmail.com>
2022-12-25 18:15:25 +0300

    Register partly

--- src/pages/components/Register/index.tsx
+++ src/pages/components/Register/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Grid, useTheme } from '@mui/material'
+import { Grid, useTheme, TextField, Typography } from '@mui/material'
 import styled from 'styled-components'
 
 const Content = styled(Grid)`
@@ -7,13 +7,32 @@ const Content = styled(Grid)`
 `
 const Register = (): JSX.Element => {
   const theme = useTheme()
-  return <Grid container direction={'row'} columns={12}>
+  return <Grid container direction={'row'} columns={12} >
     <Grid xs={0} sm={0} md={6} item>
       <Content bgcolor={theme.palette.primary.main}>
         1
       </Content>
     </Grid>
-    <Grid xs={12} sm={12} md={6} item>2</Grid>
+    <Grid xs={12} sm={12} md={6} item sx={{ pt: 4 }}>
+      <Grid container component={'form'} direction={'column'} padding={4} spacing={4} justifyContent={'center'}>
+        <Grid item>
+          <Typography variant={'h3'} color={theme.palette.primary.main}>SIGN UP</Typography>
+        </Grid>
+        <Grid item>
+          <Grid columnSpacing={2} container columns={12}>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField fullWidth={true} name={'firstName'} label={'First Name'}/>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <TextField  fullWidth={true} name={'lastName'} label={'Last Name'}/>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item >
+          <TextField fullWidth={false} sx={{ width: '85%' }} name={'email'} label={'Email'}/>
+        </Grid>
+      </Grid>
+    </Grid>
   </Grid>
 }
 
