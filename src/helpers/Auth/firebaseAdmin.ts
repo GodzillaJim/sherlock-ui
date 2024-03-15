9983ecf68ab3328a8a0c31fe27d0333000044647
Jimna Njoroge <jimnanjoroge76@gmail.com>
2024-03-15 19:48:19 +0300

    format pem, build fix

--- src/helpers/Auth/firebaseAdmin.ts
+++ src/helpers/Auth/firebaseAdmin.ts
@@ -4,7 +4,7 @@ import { getAuth } from "firebase-admin/auth";
 
 const credentials = {
   ...config,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
 };
 if (!getApps().length) {
