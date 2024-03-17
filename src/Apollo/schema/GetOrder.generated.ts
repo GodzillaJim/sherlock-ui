612e56678136245bf9383faa01802e97960ff2a4
Jimna Njoroge <jimnanjoroge76@gmail.com>
2024-03-17 22:59:33 +0300

    fetch discipline field

--- src/Apollo/schema/GetOrder.generated.ts
+++ src/Apollo/schema/GetOrder.generated.ts
@@ -1,6 +1,7 @@
 import * as common from '../../../graphql/common';
 
 import { gql } from '@apollo/client';
+import { OrderFragmentFragmentDoc } from './fragments/OrderFragment.generated';
 import * as Apollo from '@apollo/client';
 const defaultOptions = {} as const;
 export type GetOrderQueryVariables = common.Exact<{
@@ -8,87 +9,16 @@ export type GetOrderQueryVariables = common.Exact<{
 }>;
 
 
-export type GetOrderQuery = { __typename?: 'Query', getOrder?: { __typename?: 'Order', orderId: string, title: string, description?: string | null, writingStyle: common.WritingStyle, type: common.Type, numberOfPages: number, wordsPerPage?: number | null, deadline: any, createdAt?: any | null, published?: boolean | null, status: common.OrderStatus, academicLevel?: string | null, attachments: Array<{ __typename?: 'Attachment', name?: string | null, location?: string | null, key?: string | null, mimeType?: string | null } | null>, responses: Array<{ __typename?: 'OrderResponse', id: string, comments?: string | null, answer?: string | null, responseType: common.ResponseType, createdAt: any, updatedAt: any, createdBy: { __typename?: 'User', id: string, email: string, firstName?: string | null, lastName?: string | null, password?: string | null, username?: string | null, currency?: string | null, language?: string | null, orders?: Array<string | null> | null, responses?: Array<string | null> | null, uid?: string | null, picture?: string | null, iss?: string | null, aud?: string | null, auth_time?: number | null, user_id?: string | null, sub?: string | null, iat?: number | null, exp?: number | null, email_verified?: boolean | null, roles?: Array<{ __typename?: 'Role', name?: common.RoleType | null } | null> | null, timezone?: { __typename?: 'Timezone', value?: string | null, abbr?: string | null, offset?: number | null, isdst?: boolean | null, text?: string | null, utc?: Array<string | null> | null } | null, firebase?: { __typename?: 'Firebase', sign_in_provider?: string | null, identities?: { __typename?: 'Identity', googlecom?: Array<string | null> | null, email?: Array<string | null> | null } | null } | null } }>, price?: { __typename?: 'Price', amount?: number | null, clientSecret?: string | null, currency?: string | null, paymentStatus?: common.PaymentStatus | null } | null } | null };
+export type GetOrderQuery = { __typename?: 'Query', getOrder?: { __typename?: 'Order', orderId: string, title: string, description?: string | null, writingStyle: common.WritingStyle, type: common.Type, numberOfPages: number, wordsPerPage?: number | null, deadline: any, createdAt?: any | null, published?: boolean | null, status: common.OrderStatus, academicLevel?: string | null, discipline?: string | null, attachments: Array<{ __typename?: 'Attachment', name?: string | null, location?: string | null, key?: string | null, mimeType?: string | null } | null>, responses: Array<{ __typename?: 'OrderResponse', id: string, comments?: string | null, answer?: string | null, responseType: common.ResponseType, createdAt: any, updatedAt: any, createdBy: { __typename?: 'User', id: string, email: string, firstName?: string | null, lastName?: string | null, password?: string | null, username?: string | null, currency?: string | null, language?: string | null, orders?: Array<string | null> | null, responses?: Array<string | null> | null, uid?: string | null, picture?: string | null, iss?: string | null, aud?: string | null, auth_time?: number | null, user_id?: string | null, sub?: string | null, iat?: number | null, exp?: number | null, email_verified?: boolean | null, roles?: Array<{ __typename?: 'Role', name?: common.RoleType | null } | null> | null, timezone?: { __typename?: 'Timezone', value?: string | null, abbr?: string | null, offset?: number | null, isdst?: boolean | null, text?: string | null, utc?: Array<string | null> | null } | null, firebase?: { __typename?: 'Firebase', sign_in_provider?: string | null, identities?: { __typename?: 'Identity', googlecom?: Array<string | null> | null, email?: Array<string | null> | null } | null } | null } }>, price?: { __typename?: 'Price', amount?: number | null, clientSecret?: string | null, currency?: string | null, paymentStatus?: common.PaymentStatus | null } | null } | null };
 
 
 export const GetOrderDocument = gql`
     query GetOrder($orderId: String!) {
   getOrder(orderId: $orderId) {
-    orderId
-    title
-    description
-    attachments {
-      name
-      location
-      key
-      mimeType
-    }
-    writingStyle
-    type
-    numberOfPages
-    wordsPerPage
-    deadline
-    responses {
-      id
-      comments
-      answer
-      responseType
-      createdAt
-      updatedAt
-      createdBy {
-        id
-        email
-        firstName
-        lastName
-        password
-        roles {
-          name
-        }
-        username
-        currency
-        timezone {
-          value
-          abbr
-          offset
-          isdst
-          text
-          utc
-        }
-        language
-        orders
-        responses
-        uid
-        picture
-        iss
-        aud
-        auth_time
-        user_id
-        sub
-        iat
-        exp
-        email_verified
-        firebase {
-          identities {
-            googlecom
-            email
-          }
-          sign_in_provider
-        }
-      }
-    }
-    createdAt
-    published
-    price {
-      amount
-      clientSecret
-      currency
-      paymentStatus
-    }
-    status
-    academicLevel
+    ...OrderFragment
   }
 }
-    `;
+    ${OrderFragmentFragmentDoc}`;
 
 /**
  * __useGetOrderQuery__
